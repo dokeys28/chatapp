3a7a59d2b633871fc31c2efc36a8069d0501c595
dokeys <dokeys28@gmail.com>
2023-09-25 11:49:28 -0400

    recarga

--- index.js
+++ index.js
@@ -25,4 +25,6 @@ async function ver(){
 
 boton_enviar.addEventListener('click', enviar)
 document.addEventListener("DOMContentLoaded",ver)
-
+setTimeout(function() {
+    location.reload();
+}, 2000);
